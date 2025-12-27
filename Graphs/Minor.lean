1b23235c359d34ebf5bdcf8fb89c7e4d7dd4071c
Vincent Beffara <vbeffara@gmail.com>
2025-12-27 12:31:53 +0100

    Tweak lean files

--- Graphs/Minor.lean
+++ Graphs/Minor.lean
@@ -1,7 +1,10 @@
 import Mathlib
+import Graphs.Basic
+import Graphs.Contraction
 
 variable {α β : Type*}
 
-def IsMinor (G : SimpleGraph α) (H : SimpleGraph β) : Prop := sorry
+def IsMinor (G : SimpleGraph α) (H : SimpleGraph β) : Prop :=
+  ∃ (γ : Type*) (K : SimpleGraph γ), G ≼s K ∧ K ≼c H
 
 infix:50 " ≼ " => IsMinor
