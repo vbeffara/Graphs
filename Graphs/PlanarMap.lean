17d605c7641f8a677693fe47c86eb79f25f09c0a
Vincent Beffara <vbeffara@gmail.com>
2026-01-23 15:22:08 +0100

    Tweak

--- Graphs/PlanarMap.lean
+++ Graphs/PlanarMap.lean
@@ -35,8 +35,8 @@ end Fin
 structure PlanarMap (V : Type*) where
   deg : V → ℕ
   α' : Perm (Σ v, Fin (deg v))
-  α_ne e : α' e ≠ e
-  α_sq e : α' (α' e) = e
+  α'_ne e : α' e ≠ e
+  α'_sq e : α' (α' e) = e
 
 variable {G : PlanarMap V}
 
@@ -56,6 +56,10 @@ def σ (G : PlanarMap V) : Perm (darts G) where
 
 def α (G : PlanarMap V) : Perm (darts G) := G.α'
 
+@[simp] theorem α_ne {e : G.darts} : G.α e ≠ e := G.α'_ne e
+
+@[simp] theorem α_sq {e : G.darts} : G.α (G.α e) = e := G.α'_sq e
+
 def φ (G : PlanarMap V) : Perm (darts G) := (G.σ)⁻¹ * (G.α)⁻¹
 
 @[simp] theorem σαφ : G.φ * G.α * G.σ = 1 := by simp [φ]
@@ -68,30 +72,27 @@ noncomputable def n_faces [Fintype V] (G : PlanarMap V) : ℕ := by
   exact G.φ.cycleType.card + Fintype.card (fixedPoints G.φ)
 
 @[simp] theorem fixedPoints_α [Fintype V] : fixedPoints G.α = ∅ := by
-  by_contra! ; obtain ⟨e, he⟩ := this ; exact G.α_ne e he
+  by_contra! ; obtain ⟨e, he⟩ := this ; exact G.α_ne he
 
 theorem darts_even [Fintype V] : Even (Fintype.card G.darts) := by
   let S : Finset (Finset G.darts) := Finset.image (fun e => {e, G.α e}) Finset.univ
   have h1 : ∀ s ∈ S, s.card = 2 := by
     simp only [S, Finset.mem_image, Finset.mem_univ, true_and, forall_exists_index,
       forall_apply_eq_imp_iff]
-    intro e
-    exact Finset.card_pair (G.α_ne e).symm
+    exact fun e => Finset.card_pair G.α_ne.symm
   have h3 : (Finset.univ : Finset G.darts) = Finset.biUnion S id := by
     ext e; simp [S, Finset.mem_biUnion, Finset.mem_image]
-  have h4 : (S : Set (Finset G.darts)).PairwiseDisjoint id := by
-    simp [S]
+  rw [← Finset.card_univ, h3, Finset.card_biUnion]
+  · apply Finset.even_sum _ (by grind)
+  · simp only [Finset.coe_image, Finset.coe_univ, Set.image_univ, S]
     rintro d1 ⟨e1, rfl⟩ d2 ⟨e2, rfl⟩
     simp [Finset.disjoint_left]
-    have s1 : ∀ e, G.α e ≠ e := G.α_ne
-    have s2 : ∀ e, G.α (G.α e) = e := G.α_sq
-    grind
-  simpa [← Finset.card_univ, h3, Finset.card_biUnion h4] using Finset.even_sum _ (by grind)
+    grind [α_sq]
 
 noncomputable def χ [Fintype V] (G : PlanarMap V) : ℤ :=
   G.n_vertices - G.n_edges + G.n_faces
 
-example [Fintype V] : Even G.χ := by
+example [Fintype V] : Even G.χ := by -- this is only true for connected planar maps
   sorry
 
 noncomputable def genus [Fintype V] (G : PlanarMap V) : ℤ :=
