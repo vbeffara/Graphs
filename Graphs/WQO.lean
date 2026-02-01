16a36b85bf27428419b5df17dcedfee16f969bdc
Razortant <razortant.azerty@gmail.com>
2026-02-01 15:26:31 +0100

    Edit QO_tricolor theorem in WQO.lean

--- Graphs/WQO.lean
+++ Graphs/WQO.lean
@@ -2,8 +2,15 @@ import Mathlib
 
 variable {α : Type*} [PartialOrder α]
 
-theorem QO_tricolor {X : Type*} [Preorder X] {f : ℕ → X} : ∃ g : ℕ → ℕ, (∀ n : ℕ, g n ≤ g (n + 1)) ∧ ((∀ i : ℕ, ∀ j : ℕ, f (g i) ≤ f (g j)) ∨ (∀ i : ℕ, ∀ j : ℕ, f (g i) > f (g j)) ∨ (∀ i : ℕ, ∀ j : ℕ, ¬ ((f (g i) ≤ f (g j)) ∨ (f (g i) > f (g j))))) := by
-  sorry
+theorem QO_tricolor {X : Type*} [Preorder X] {f : ℕ → X} : ∃ g : ℕ → ℕ, (∀ n : ℕ, g n < g (n + 1)) ∧ ((∀ i : ℕ, ∀ j : ℕ, f (g i) ≤ f (g j)) ∨ (∀ i : ℕ, ∀ j : ℕ, f (g i) > f (g j)) ∨ (∀ i : ℕ, ∀ j : ℕ, ¬ ((f (g i) ≤ f (g j)) ∨ (f (g i) > f (g j))))) := by
+  let K := SimpleGraph.completeGraph ℕ
+  let C : Finset ℕ := {0, 1, 2}
+  let c : K.EdgeLabeling C := fun e =>
+    let s := e.val
+
+    match s with
+    |
+  have hramsey := ramsey2
 
 theorem StrictAnti_iff_descending {X : Type*} [Preorder X] {f : ℕ → X} :
     StrictAnti f ↔ ∀ n, f (n + 1) < f n := by
