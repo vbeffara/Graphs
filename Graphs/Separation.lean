0b4bd21b48162b06bc00a023341e6660263dcd06
Vincent Beffara <vbeffara@gmail.com>
2026-02-27 12:18:02 +0100

    Tweaks

--- Graphs/Separation.lean
+++ Graphs/Separation.lean
@@ -1,8 +1,8 @@
 import Mathlib
 
-open SimpleGraph
+open SimpleGraph Set
 
-variable {V : Type*}
+variable {V : Type*} {G : SimpleGraph V} {A B S : Set V}
 
 namespace SimpleGraph
 
@@ -11,5 +11,9 @@ A set of vertices S separates A from B in G if every A-B path in G contains a ve
 -/
 def Separates (G : SimpleGraph V) (A B : Set V) (S : Set V) : Prop :=
   ∀ u ∈ A, ∀ v ∈ B, ∀ p : G.Walk u v, ∃ x ∈ p.support, x ∈ S
+
+theorem separates_cover (h : A ∪ B = univ) : G.Separates A B S ↔
+    ((A ∩ B ⊆ S) ∧ (∀ u ∈ A, ∀ v ∈ B, G.Adj u v → u ∈ S ∨ v ∈ S)) := by
+  sorry
 
 end SimpleGraph
