c5cf08e0084de6c2868a9f60461e880f5db2140c
Vincent Beffara <vbeffara@gmail.com>
2026-03-11 23:26:56 +0100

    Golf

--- Graphs/RTree.lean
+++ Graphs/RTree.lean
@@ -18,24 +18,15 @@ lemma map_tail {V W : Type*} {a b : V} {G : SimpleGraph V} {G' : SimpleGraph W} 
 def subtree (T : RTree) (r : T.T.neighborSet T.root) : RTree := by
   let V : Set T.support := {T.root}ᶜ
   set G : SimpleGraph V := T.T.induce V
-  let g : V := ⟨r, r.prop.ne.symm⟩
-  let C := G.connectedComponentMk g
-  refine ⟨⟨g, ConnectedComponent.connectedComponentMk_mem⟩, C.toSimpleGraph, ?_⟩
+  let C := G.connectedComponentMk ⟨r, r.prop.ne.symm⟩
+  refine ⟨⟨_, ConnectedComponent.connectedComponentMk_mem⟩, C.toSimpleGraph, ?_⟩
   constructor
   · exact C.connected_toSimpleGraph
-  · let f1 : C.toSimpleGraph →g G := C.toSimpleGraph_hom
-    let f2 : G →g T.T.induce ⊤ := induceHom .id (by tauto)
-    let f3 : T.T.induce ⊤ →g T.T := induceUnivIso T.T
-    let f : C.toSimpleGraph →g T.T := f3.comp (f2.comp f1)
-    have hf : Injective f := by
-      intro x y
-      simp [f, f1, f2, f3, induceHom, ConnectedComponent.toSimpleGraph_hom]
-      grind
+  · let f := (induceUnivIso T.T).toHom |>.comp (induceHom .id (by tauto)) |>.comp C.toSimpleGraph_hom
+    have hf : Injective f := by intro x y h ; ext ; exact h
     rintro u p
-    have key := T.isTree.2
-    let q := p.map f
-    have := key q
-    simp [q, Walk.isCycle_iff_isPath_tail_and_le_length] at this ⊢
+    have := T.isTree.2 (p.map f)
+    simp [Walk.isCycle_iff_isPath_tail_and_le_length] at this ⊢
     grind [Walk.map_isPath_of_injective]
 
 def subtrees (T : RTree) : Set RTree := range (subtree T)
